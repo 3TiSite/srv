cce22b7cb7974f10f28e5e8a939805022bb03ff2
3Ti <3Ti.Site@gmail.com>
2024-01-05 07:16:21 +0800

    ♨

--- rust/api/src/main.rs
+++ rust/api/src/main.rs
@@ -22,37 +22,37 @@ async fn main() -> anyhow::Result<()> {
   let mut router = Router::new();
 
   macro_rules! req {
-        // (=> $func:ident) => {
-        //     post!("", $func)
-        // };
-        ($method:ident $wrap:ident  $($url:ident),+) => {
-            req!($method $wrap $($url=>$url);+)
-        };
-        ($method:ident $wrap:ident $($url:stmt => $func:ident);+) => {
-            $(
-                req!(
-                    $method
-                    $wrap
-                    const_str::replace!(
-                        const_str::replace!(
-                            const_str::unwrap!(const_str::strip_suffix!(stringify!($url), ";")),
-                            " ",
-                            ""
-                        ),
-                        "&",
-                        ":"
-                    ),
-                    $func
-                );
-            )+
-        };
-        ($method:ident $wrap:ident $url:expr, $func:ident) => {
-            router = router.route(
-                const_str::concat!('/', $url),
-                routing::$method(t3::$wrap(url::$func::$method)),
-            )
-        };
-    }
+    // (=> $func:ident) => {
+    //     post!("", $func)
+    // };
+    ($method:ident $wrap:ident  $($url:ident),+) => {
+        req!($method $wrap $($url=>$url);+)
+    };
+    ($method:ident $wrap:ident $($url:stmt => $func:ident);+) => {
+        $(
+          req!(
+              $method
+              $wrap
+              const_str::replace!(
+                  const_str::replace!(
+                      const_str::unwrap!(const_str::strip_suffix!(stringify!($url), ";")),
+                      " ",
+                      ""
+                  ),
+                  "&",
+                  ":"
+              ),
+              $func
+          );
+        )+
+    };
+    ($method:ident $wrap:ident $url:expr, $func:ident) => {
+        router = router.route(
+          const_str::concat!('/', $url),
+          routing::$method(t3::$wrap(url::$func::$method)),
+        )
+    };
+  }
 
   route!();
 
