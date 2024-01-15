db821cae1a88f8699c03d8e2d8c04e6690bc2ccf
3Ti <3Ti.Site@gmail.com>
2024-01-15 14:21:11 +0800

    ♨

--- rust/lib/user/src/cookie_set.rs
+++ rust/lib/user/src/cookie_set.rs
@@ -1,8 +1,14 @@
+use chrono::{DateTime, NaiveDateTime, Utc};
 use sk::sk;
 use xhash::hash64;
 
 /// 对 BASE 求余, 为了防止数字过大,
 pub const BASE: u64 = 1024;
+
+fn ts2gmt(ts: u64) -> String {
+  let datetime: DateTime<Utc> = DateTime::from_utc(NaiveDateTime::from_ts(ts as _, 0), Utc);
+  datetime.to_rfc2822()
+}
 
 pub fn cookie_set(host: &str, client_id: u64) -> [String; 1] {
   let now = sts::sec();
@@ -11,8 +17,10 @@ pub fn cookie_set(host: &str, client_id: u64) -> [String; 1] {
   let token = [&hash64(&[sk(), t].concat()).to_le_bytes()[..], t].concat();
   let i = cookiestr::e(token);
   let max_age = 34560000;
-  let expire = now + max_age;
+  let expire = ts2gmt(now + max_age);
 
-  let age = format!(";max-age={max_age};domain={host};path=/;Partitioned;Secure;SameSite=None");
+  let age = format!(
+    ";expires={expire};max-age={max_age};domain={host};path=/;Partitioned;Secure;SameSite=None"
+  );
   [format!("I={i}{age};HttpOnly")]
 }
