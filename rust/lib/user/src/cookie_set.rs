3389ea21fa73eb281cb85676c58c470b6530fc5d
3Ti <3Ti.Site@gmail.com>
2024-01-15 14:17:50 +0800

    ♨

--- rust/lib/user/src/cookie_set.rs
+++ rust/lib/user/src/cookie_set.rs
@@ -4,14 +4,15 @@ use xhash::hash64;
 /// 对 BASE 求余, 为了防止数字过大,
 pub const BASE: u64 = 1024;
 
-pub fn day10() -> u64 {
-  (sts::sec() / (86400 * 10)) % BASE
-}
-
 pub fn cookie_set(host: &str, client_id: u64) -> [String; 1] {
-  let t = &vb::e([day10(), client_id])[..];
+  let now = sts::sec();
+  let day = (now / (86400 * 10)) % BASE;
+  let t = &vb::e([day, client_id])[..];
   let token = [&hash64(&[sk(), t].concat()).to_le_bytes()[..], t].concat();
   let i = cookiestr::e(token);
-  let age = format!(";max-age=99999999;domain={host};path=/;Partitioned;Secure;SameSite=None");
+  let max_age = 34560000;
+  let expire = now + max_age;
+
+  let age = format!(";max-age={max_age};domain={host};path=/;Partitioned;Secure;SameSite=None");
   [format!("I={i}{age};HttpOnly")]
 }
